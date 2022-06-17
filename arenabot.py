4a2e7e0a88fbd9cb18a61bf05ea47fc97de34aac
wnbilly <weynansbilly@gmail.com>
2022-06-17 15:13:33 +0200

    1er_test

--- arenabot.py
+++ arenabot.py
@@ -5,6 +5,11 @@ import sys
 import inspyred
 import random
 import math
+import numpy as np
+
+random_number_generator = random.Random()
+random_number_generator.seed(42) # remember, seeding the generators with a fixed value ensures that you will always obtain the same sequence of numbers at every run 
+
 
 '''This function accepts in input a list of strings, and tries to parse them to update the position of a robot. Then returns distance from objective.'''
 def fitnessRobot(listOfCommands, visualize=False) :
@@ -73,6 +78,68 @@ def fitnessRobot(listOfCommands, visualize=False) :
 		plt.show()
 
 	return distanceFromObjective
+
+
+# this function generates initial random commands for the robot; it needs a random number generator (called "random") and a dictionary or arguments in input,
+# to be used by the EvolutionaryComputation object; returns one (randomly generated) candidate individual
+def generator_commands(random, args) :
+	minimum_angle = args["minimum_angle"] # also, the minimum value of each dimension will be specified later in "args"
+	maximum_angle = args["maximum_angle"] # same goes for the maximum value
+
+	minimum_distance = args["minimum_distance"] # also, the minimum value of each dimension will be specified later in "args"
+	maximum_distance = args["maximum_distance"] # same goes for the maximum value
+
+	max_individual_length = args["max_individual_length"]//2
+
+	# the individual will be a series of "number_of_dimensions" random values, generated between "minimum" and "maximum"
+	# the individual will be a series of commands (move and rotate)
+
+	individual_length = random_number_generator.randint(1, max_individual_length)
+	individual = np.zeros(individual_length)
+
+	for i in range(0, individual_length):
+		individual[i] = random_number_generator.uniform(minimum_angle, maximum_angle)
+		individual[i] = random_number_generator.uniform(minimum_distance, maximum_distance)
+
+	return individual
+
+
+# instantiate the evolutionary algorithm object
+evolutionary_algorithm = inspyred.ec.EvolutionaryComputation(random_number_generator)
+# and now, we specify every part of the evolutionary algorithm
+evolutionary_algorithm.selector = inspyred.ec.selectors.tournament_selection # by default, tournament selection has tau=2 (two individuals), but it can be modified (see below)
+evolutionary_algorithm.variator = [inspyred.ec.variators.uniform_crossover, inspyred.ec.variators.gaussian_mutation] # the genetic operators are put in a list, and executed one after the other
+evolutionary_algorithm.replacer = inspyred.ec.replacers.plus_replacement # "plus" -> "mu+lambda"
+evolutionary_algorithm.terminator = inspyred.ec.terminators.evaluation_termination # the algorithm terminates when a given number of evaluations (see below) is reached
+
+# evolutionary_algorithm.observer = inspyred.ec.observers.best_observer # prints best individual to screen
+# evolutionary_algorithm.observer = inspyred.ec.observers.stats_observer # print out population statistics
+evolutionary_algorithm.observer = inspyred.ec.observers.plot_observer # plots evolution
+final_population = evolutionary_algorithm.evolve( 
+                               generator = generator_commands, # of course, we need to specify the evaluator
+                               evaluator = fitnessRobot, # and the corresponding evaluator
+                               pop_size = 100, # size of the population
+                               num_selected = 200, # size of the offspring (children individuals)
+                               maximize = False, # this is a minimization problem, but inspyred can also manage maximization problem
+                               max_evaluations = 2000, # maximum number of evaluations before stopping, used by the terminator
+                               tournament_size = 2, # size of the tournament selection; we need to specify it only if we need it different from 2
+                               crossover_rate = 1.0, # probability of applying crossover
+                               mutation_rate = 0.1, # probability of applying mutation
+                               
+                               # all arguments specified below, THAT ARE NOT part of the "evolve" method, will be automatically placed in "args"
+                               max_individual_length = 50, # number of dimensions of the problem, used by "generator_weierstrass"
+                               minimum_angle = -90, # minimum angle for generator_commands
+                               maximum_angle = 90, # maximum angle
+							   minimum_distance = 0, # minimum distance for generator_commands
+							   maximum_distance = 1000, # minimum angle
+                              )
+
+# after the evolution is over, the resulting population is stored in "final_population"; the best individual is on the top
+best_individual = final_population[0]
+print("The best individual has fitness %.2f" % best_individual.fitness)
+
+
+
 
 ################# MAIN
 def main() :
